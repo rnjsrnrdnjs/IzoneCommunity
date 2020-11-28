6fe408f08cd0e53158bc89e7f100f1613196f537
rnjsrnrdnjs <49832278+rnjsrnrdnjs@users.noreply.github.com>
2020-11-28 23:58:04 +0900

    Add files via upload

--- izeOneWeb/js/twoIzone.js
+++ izeOneWeb/js/twoIzone.js
@@ -6,23 +6,21 @@ function twostart() {
   $("#header").load("header.html");
 
   var navChild=elt("div",{id:"nav"});
-  navChild.style.width=30+"vw";
-  navChild.style.height=65+"vh";
-  navChild.style.float="left";
   document.body.appendChild(navChild);
   $("#nav").load("nav.html");
   
   var sectionChild=elt("div",{id:"section"});
-  sectionChild.style.width=50+"vw";
-  sectionChild.style.height=65+"vh";
-  sectionChild.style.float="left";
+  sectionChild.style.width=100+"vw";
+  sectionChild.style.height=400+"vh";
   document.body.appendChild(sectionChild);
   $("#section").load("section.html");
   
   var footerChild=elt("div",{id:"footer"});
   footerChild.style.width=100+"vw";
-  footerChild.style.height=15+"vh";
+  footerChild.style.height=200+"px";
+  footerChild.style.position="absolute";
   footerChild.style.clear="left";
+  footerChild.style.backgroundColor="#81ffee";
   document.body.appendChild(footerChild);
   $("#footer").load("footer.html");
 }
--- izeOneWeb/js/windowOnload.js
+++ izeOneWeb/js/windowOnload.js
@@ -1,4 +1,5 @@
 //자바스크립트를이용한 내용물 표기
 window.onload=function(){
   reboot(document.body);
+  includeRouter();
 };
